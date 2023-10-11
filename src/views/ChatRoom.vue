62498e01492fdd410fbbee6e26220a77c0d87556
esmanurbtmz <esmabitmez@hotmail.com>
2023-10-11 10:21:52 +0300

    Worked on the welcome component design.

--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -3,8 +3,18 @@
 </template>
 
 <script>
+import { watch } from 'vue'
+import getUser from "../composables/getUser"
 export default {
-
+  created(){
+    const {user} = getUser()
+    
+    watch(user, ()=>{
+      if(!user.value){
+        $router.push({name: "Welcome"})
+      }
+    })
+  }
 }
 </script>
 
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,14 +1,49 @@
 <template>
-  <div>welcome
+  <div class="main-container d-flex align-items-center ">
+    <div class="container">
+      <div class="beginning-page pt-4 pb-4 row flex-wrap d-flex justify-content-center align-items-center">
+        <div class="col-lg-5">
+          <div class="main-photo">
+            <img src="../assets/medias/2937927.jpg" alt="">
+          </div>
+        </div>
+        <div class="col-lg-5">
+          <login></login>
+        </div>
+      </div>
+    </div>
+    
   </div>
 </template>
 
 <script>
+import Login from '../components/Login.vue'
 export default {
+  components: { Login },
 
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+$body-color : #a8dadc;
+$white-color: #fff;
+.main-container{
+  height: 100vh;
+  background-color:$body-color;
+}
+.beginning-page{
+  background-color: $white-color;
+  border-radius: 20px;
+}
+.main-photo{
+  width: 100%;
+  height: 60vh;
+  img{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 20px;
+  }
+ 
+}
 </style>
